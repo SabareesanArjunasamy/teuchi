28ea965adf31d1ca85129f26399b67183bd7776a
Sabareesan Arjunasamy <sab@ajirasoft.com>
2022-11-07 02:21:32 +0530

    ping pong  test

---
+++ bot.js
@@ -0,0 +1,20 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+require('dotenv').config()
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
+    if (interaction.commandName === 'naruto') {
+        await interaction.reply('Ramen...');
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
